dc05c394a3219ad9d4b31ae3a71af2d56805d626
WhYouDied <brunov_artem@mail.ru>
2017-11-03 02:14:00 +0300

    SpralDigits

--- python/SpiralDigits.py
+++ python/SpiralDigits.py
@@ -1,20 +1,83 @@
 M,N = eval(input())
 
-def Colculate(M,N):
-	A = [[0] * M for i in range(N)]
-		 
-			
-	return A
-def Generator(M,N):
-	i = 0
-	A = Colculate(M,N)
-	while i < M*N:
-		yield A[i]
+def right(A,i,j,p,q,n):
+	s = 0
+	while s < q:
+		A[i][j] = p
+		p+=1
+		j+=1
+		s+=1
+		n+=1
+		if p == 10:
+			p = 0
+	return A, p, n, i+1, j-1
+
+def down(A,i,j,p,b,n):
+	s = 0
+	while s < b:
+		A[i][j] = p
+		p+=1
+		s+=1
 		i+=1
-d = 1
-for i in Generator(M,N):
-	print(i, end = ' ')
-	if d == M:
-		print('')
-		d = 0
-	d+=1
+		n+=1
+		if p == 10:
+			p = 0
+	return A, p, n, i-1, j-1
+
+def left(A,i,j,p,q,n):
+	s = 0
+	while s < q:
+		A[i][j] = p
+		p+=1
+		j-=1
+		s+=1
+		n+=1
+		if p == 10:
+			p = 0
+	return A, p, n, i-1, j+1
+
+def up(A,i,j,p,b,n):
+	s = 0
+	while s < b:
+		A[i][j] = p
+		p+=1
+		i-=1
+		s+=1
+		n+=1
+		if p == 10:
+			p = 0
+	return A, p, n, i + 1, j+1
+
+A = [[0] * M for i in range(N)]
+
+n = 0
+j = 0
+i = 0
+p = 0
+q = M
+b = N 
+while 1:
+	A,p,n,i,j = right(A,i,j,p,q,n)
+	b-=1
+	if n == N*M:
+		break
+	A,p,n,i,j = down(A,i,j,p,b,n)
+	q-=1
+	if n == M*N:
+		break
+	A,p,n,i,j = left(A,i,j,p,q,n)
+	b-=1
+	if n == M*N:
+		break
+	A,p,n,i,j = up(A,i,j,p,b,n)
+	q-=1
+	if n == N*M:
+		break
+i = 0
+while i < N:
+	j = 0
+	while j < M:
+		print(A[i][j], end = " ")
+		j+=1
+	print("")
+	i+=1

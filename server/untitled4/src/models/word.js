adc4876cc9e2707102c6058ff01bbd069599a3c5
WhYouDied <brunov_artem@mail.ru>
2017-12-20 11:34:41 +0300

    erver word

--- server/untitled4/src/models/word.js
+++ server/untitled4/src/models/word.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const wordSchema = new Schema({
-    company: { type: Schema.ObjectId, ref: "Company", required: true },
+    playlist: { type: Schema.ObjectId, ref: "Playlist", required: true },
     name: {type: String, required: true, unique: true},
 
     fullName: String,
